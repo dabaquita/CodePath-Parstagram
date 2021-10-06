705d8aa1b4ad1ce7575c3d8b4c5b03a75719bbc9
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-10-06 12:36:17 -0400

    Formatted camera vc

--- CodePath-Parstagram/ViewControllers/CameraViewController.swift
+++ CodePath-Parstagram/ViewControllers/CameraViewController.swift
@@ -8,22 +8,88 @@
 import UIKit
 
 class CameraViewController: UIViewController {
+    
+    let mainVerticalStackView = UIStackView()
+    let postImageView = UIImageView()
+    let commentTextField = UITextField()
+    let submitButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Submit", for: .normal)
+        button.addTarget(
+            self,
+            action: #selector(didTapSubmit(_:)),
+            for: .touchUpInside
+        )
+        button.clipsToBounds = true
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        configureStackView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: Configure Subviews
+    private func configureStackView() {
+        mainVerticalStackView.axis = .vertical
+        mainVerticalStackView.distribution = .fillProportionally
+        mainVerticalStackView.alignment = .fill
+        mainVerticalStackView.spacing = 21
+        view.addSubview(mainVerticalStackView)
+        
+        // Constraints
+        mainVerticalStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mainVerticalStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 11),
+            mainVerticalStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 11),
+            mainVerticalStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -11),
+            mainVerticalStackView.heightAnchor.constraint(equalToConstant: view.bounds.height / 2)
+        ])
+        
+        // Arranged subviews
+        configurePostImageView()
+        configureCommentTextField()
+        mainVerticalStackView.addArrangedSubview(submitButton)
     }
-    */
+    
+    private func configurePostImageView() {
+        postImageView.image = UIImage(named: "image_placeholder")
+        postImageView.contentMode = .scaleAspectFit
+        postImageView.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapCameraImageView(_:)))
+        postImageView.addGestureRecognizer(tapGesture)
+        mainVerticalStackView.addArrangedSubview(postImageView)
+    }
+    
+    private func configureCommentTextField() {
+        commentTextField.placeholder = "Comment..."
+        commentTextField.borderStyle = .roundedRect
+        mainVerticalStackView.addArrangedSubview(commentTextField)
+    }
+    
+    
+    // MARK: Selectors
+    @objc func didTapSubmit(_ sender: Any) {
+        print("Tapped submit")
+    }
+    
+    @objc func didTapCameraImageView(_ sender: Any) {
+        print("Tapped camera")
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        self.present(picker, animated: true)
+    }
+}
 
+extension CameraViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
 }
