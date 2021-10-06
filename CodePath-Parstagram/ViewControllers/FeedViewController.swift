4793dd1961d46279f4bea2ddf166d7558fb4ee30
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-10-06 12:35:59 -0400

    Connected feed to camera vc

--- CodePath-Parstagram/ViewControllers/FeedViewController.swift
+++ CodePath-Parstagram/ViewControllers/FeedViewController.swift
@@ -27,18 +27,7 @@ class FeedViewController: UIViewController {
     
     // MARK: Selectors
     @objc func didTapCamera(_ sender: Any) {
-        print("Tapped Camera")
+        let cameraVC = CameraViewController()
+        self.show(cameraVC, sender: self)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
